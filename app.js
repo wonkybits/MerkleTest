44e610ddd8119b42a1ebae3b4eab0355f770058e
Phillip Stene <phil.stene@gmail.com>
2021-07-22 00:17:18 -0400

    added backend validation and sanitization

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const UserModel= require('./model');
-const escapeRegex = require('escape-string-regexp');
+const { check, validationResult } = require('express-validator');
 
 const DBURL = process.env.mongodbURL || 'mongodb://127.0.0.1:27017/MerkleUsers';
 
@@ -15,8 +15,78 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(DBURL, { useUnifiedTopology: true, useNewUrlParser: true });
+// validation checks
+const profileValidate = [
+    check('firstname')
+        .exists()
+        .withMessage('firstname is Required')
+        .isAlpha("en-US", { ignore: " " })
+        .withMessage('firstname wrong format')
+        .trim()
+        .escape(),
+    check('lastname')
+        .exists()
+        .withMessage('lastname is Required')
+        .isAlpha("en-US", { ignore: " " })
+        .withMessage('lastname wrong format')
+        .trim()
+        .escape(),
+    check('address1')
+        .exists()
+        .withMessage('address1 is Required')
+        .isAlphanumeric("en-US", { ignore: " " })
+        .withMessage('address1 wrong format')
+        .trim()
+        .escape(),
+    check('address2')
+        .optional({ nullable: true, checkFalsy: true})
+        .isAlphanumeric("en-US", { ignore: " " })
+        .withMessage('address2 wrong format')
+        .trim()
+        .escape(),
+    check('city')
+        .exists()
+        .withMessage('city is Required')
+        .isAlpha("en-US", { ignore: " " })
+        .withMessage('city wrong format')
+        .trim()
+        .escape(),
+    check('state')
+        .exists()
+        .withMessage('state is Required')
+        .isAlpha("en-US", { ignore: " " })
+        .withMessage('state wrong format')
+        .trim()
+        .escape(),
+    check('zip')
+        .exists()
+        .withMessage('zip is Required')
+        .isPostalCode('US')
+        .withMessage('zip is not valid')
+        .trim()
+        .escape(),
+    check('country')
+        .exists()
+        .withMessage('country is Required')
+        .isAlpha()
+        .withMessage('country not in text form')
+        .isLength({ min: 2, max: 2 })
+        .withMessage('country is incorrect format, use only 2 letters')
+        .isLocale()
+        .withMessage('country not a valid locale')
+        .trim()
+        .escape(),
+    check('date')
+        .exists()
+        .withMessage('date is Required')
+        .isISO8601()
+        .withMessage('date is not valid, format is YYYY-MM-DD')
+        .trim()
+        .escape()
+];
 
+//Database connection
+mongoose.connect(DBURL, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', () => {
     console.error("DB connection failed.");
@@ -25,42 +95,33 @@ db.once('open', () => {
     console.log("DB connection established.");
 });
 
-function escapeRegExp(input) {
-    const source = typeof input === 'string' || input instanceof String ? input : '';
-    return source.replace(/[*+?^${}()|[\]\\]/g, '\\$&');
-};
-
+// Routes
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.post('/confirm', (req, res) => {
-    UserModel.count({ firstname:req.body.firstname, lastname: req.body.lastname }, (err, count) => {
-        if(err) console.error(err);
-        if(count > 0) {
-            res.render('confirm', { data: "User already exists" });
-        } else {
-            req.body.date = Date.now();
-
-            const newUser = new UserModel(
-                {
-                    firstname: escapeRegExp(req.body.firstname),
-                    lastname: escapeRegExp(req.body.lastname),
-                    address1: escapeRegExp(req.body.address1),
-                    address2: escapeRegExp(req.body.address2),
-                    city: escapeRegExp(req.body.city),
-                    state: escapeRegExp(req.body.state),
-                    zip: escapeRegExp(req.body.zip),
-                    country: escapeRegExp(req.body.country),
-                    date: Date.now()
-                }
-            );
-            newUser.save((err, user) => {
-                if(err) return console.error(err);
-                res.render('confirm', { data: "" + user.firstname + " " + user.lastname + " has been registered." });
-            });
-        }
-    });
+app.post('/confirm', profileValidate, (req, res) => {
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // return res.status(422).json({ errors: errors.array() });
+        res.render('error');
+    }
+    else {
+        UserModel.count({ firstname:req.body.firstname, lastname: req.body.lastname }, (err, count) => {
+            if(err) console.error(err);
+            if(count > 0) {
+                res.render('confirm', { data: "User already exists" });
+            } else {
+                const newUser = new UserModel(req.body);
+                console.log(req.body);
+                newUser.save((err, user) => {
+                    if(err) return console.error(err);
+                    res.render('confirm', { data: "" + user.firstname + " " + user.lastname + " has been registered." });
+                });
+            }
+        });
+    }
 });
 
 app.get('/admin', (req, res) => {
